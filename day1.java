b8d9fbe64ac9667be2ba3f7f74f8ed9c1d32bd44
purwanshi <135806772+purwanshi@users.noreply.github.com>
2025-04-20 07:36:58 +0530

    day1.java

--- day1.java
+++ day1.java
@@ -1,33 +1,18 @@
 // ## DAY 1 📌 Problem: Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
     
-    public static void main(string args[]){
-    solution(newCommand(){
-        @override
-     public void execute(){
-        {
-            system.out.println("day 1");
-        }
-     }
-},1000);
-
-solution(()-> System.out.println("day 1"),1000);
-}
-
-public static void solution (Command command, int n){
-    new java.util.Timer().schedule(new java.util.TimeTask() {
+public static void main(String args[]) {
+    solution(new Command() {
         @Override
-        public void run() public static void main(String[] args){
-    solution(new Command(){
-        @Override
-        public void execute(){
+        public void execute() {
             System.out.println("day 1");
         }
     }, 1000);
 
+
     solution(() -> System.out.println("day 1"), 1000);
 }
 
-public static void solution(Command command, int n){
+public static void solution(Command command, int n) {
     new java.util.Timer().schedule(new java.util.TimerTask() {
         @Override
         public void run() {
@@ -36,15 +21,6 @@ public static void solution(Command command, int n){
     }, n);
 }
 
-interface Command{
-    void execute();
-{
-            command.execute();
-        }
-    },n);
-
-}
-interface Command{
+interface Command {
     public void execute();
-       }
 }
