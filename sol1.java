6fa2aa1148a3a88bbf5d526e949e56e7c9f04aca
purwanshi <135806772+purwanshi@users.noreply.github.com>
2025-04-20 01:56:38 +0000

    Add files via upload

---
+++ sol1.java
@@ -0,0 +1,48 @@
+public static void main(string args[]){
+    solution(newCommand(){
+        @override
+     public void execute(){
+        {
+            system.out.println("day 1");
+        }
+     }
+},1000);
+
+solution(()-> System.out.println("day 1"),1000);
+}
+
+public static void solution (Command command, int n){
+    new java.util.Timer().schedule(new java.util.TimeTask() {
+        @Override
+        public void run() public static void main(String[] args){
+    solution(new Command(){
+        @Override
+        public void execute(){
+            System.out.println("day 1");
+        }
+    }, 1000);
+
+    solution(() -> System.out.println("day 1"), 1000);
+}
+
+public static void solution(Command command, int n){
+    new java.util.Timer().schedule(new java.util.TimerTask() {
+        @Override
+        public void run() {
+            command.execute();
+        }
+    }, n);
+}
+
+interface Command{
+    void execute();
+{
+            command.execute();
+        }
+    },n);
+
+}
+interface Command{
+    public void execute();
+       }
+}
